10a8711cdd9407c1b7fe27ef0b3a695b9710ec3e
kathanon <cower@cower.se>
2025-04-24 20:18:28 +0200

    Minor cleanup.

--- ScrollCharacterCreation/Patches.cs
+++ ScrollCharacterCreation/Patches.cs
@@ -1,12 +1,9 @@
 ﻿using Cairo;
 using HarmonyLib;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 using Vintagestory.API.Client;
 using Vintagestory.GameContent;
 
